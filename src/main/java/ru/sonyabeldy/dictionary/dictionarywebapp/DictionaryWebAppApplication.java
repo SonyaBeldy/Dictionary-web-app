b9e78a58548ff0ffe94770a5772f6cfd660548cd
Sonya <sofya.beldy@mail.ru>
2023-07-01 22:51:16 +1000

    application properties origin

---
+++ src/main/java/ru/sonyabeldy/dictionary/dictionarywebapp/DictionaryWebAppApplication.java
@@ -0,0 +1,13 @@
+package ru.sonyabeldy.dictionary.dictionarywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictionaryWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sonyabeldy/dictionary/dictionarywebapp/DictionaryWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sonyabeldy.dictionary.dictionarywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
